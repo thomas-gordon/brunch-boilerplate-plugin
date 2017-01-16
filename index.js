5d1851dbbde2354fc5c6b0e09503faad7ba72c8d
Tom Gordon <tom.gordon@squareweave.com.au>
2017-01-16 11:03:52 +1100

    Catch errors properly

--- index.js
+++ index.js
@@ -3,19 +3,26 @@
 const SVGO = require('svgo');
 
 class SVGOBrunch {
-  constructor(config) {
-    this.options = Object.assign({}, config.plugins && config.plugins.svgo || {});
-  }
-  compileStatic({data}) {
-    const svgo = new SVGO(this.options);
-    return new Promise(resolve => {
-      svgo.optimize(data, result => {
-        data = result.data;
-        resolve(data);
-      });
-    });
-  }
+    constructor(config) {
+        this.options = Object.assign({}, config.plugins && config.plugins.svgo || {});
+    }
+
+    compileStatic({path, data}) {
+        let optimized,error;
+        const svgo = new SVGO(this.options);
+        try {
+            optimized = svgo.optimize(data);
+        } catch (e) {
+            error = 'SVG minification failed on ' + path + ': ' + e;
+        } finally {
+            if (error) return Promise.reject(error);
+            const result = optimized.data;
+            return Promise.resolve(result)
+        }
+
+    }
 }
+
 
 SVGOBrunch.prototype.brunchPlugin = true;
 SVGOBrunch.prototype.staticTargetExtension = 'svg';
